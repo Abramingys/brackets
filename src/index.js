31cc4757049b108c0c0247574db72bf2ba5216d6
Abramingys <abramingys@gmail.com>
2024-08-26 21:10:50 +0300

    added: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let bracketsMap = {};
+
+  bracketsConfig.forEach((pair) => {
+    bracketsMap[pair[1]] = pair[0];
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+
+    let openBracket = bracketsMap[currentChar];
+    let isSameBracketPair = openBracket === currentChar;
+
+    if (
+      openBracket &&
+      (!isSameBracketPair || stack[stack.length - 1] === openBracket)
+    ) {
+      if (stack.length === 0 || stack.pop() !== openBracket) {
+        return false;
+      }
+    } else {
+      stack.push(currentChar);
+    }
+  }
+
+  return stack.length === 0;
+};
